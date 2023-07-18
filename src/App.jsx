299be203ee389b3149619f4046b7a93cfc4a1fe1
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-18 22:03:18 +0330

    feat: add header and main title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,69 @@
+import { 
+Typography,
+AppBar,
+Card,
+CardActions,
+CardContent,
+CardMedia,
+CssBaseline,
+Grid,
+Toolbar,
+Button,
 
-import { Typography } from '@mui/material'
+Container } from '@mui/material';
+import {PhotoCamera} from "@mui/icons-material";
+import{ makeStyles} from '@material-ui/styles';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding:  "200px"
+    
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  
 
   return (
-      <div>
-        <Typography variant='h1'> hello world! </Typography>
-      </div>
+      <>
+        <CssBaseline/>
+        <AppBar position='relative'>
+          <Toolbar>
+            <PhotoCamera/>
+            <Typography variant='h6'>
+              photo Album
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Container className={classes.container} maxWidth="sm">
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Photo ALbum
+              </Typography> 
+              <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                this is photo album of muscle cars
+              </Typography>
+              <div>
+                <Grid container spacing={2} justifyContent='center'>
+                  <Grid item>
+                    <Button variant='contained' color='primary'>
+                        see more photos
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant='outlined' color='primary'>
+                        more
+                    </Button>
+                  </Grid>
+
+                </Grid>
+              </div>
+          </Container>
+        </main>
+      </>
   )
 }
 
