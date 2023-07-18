7eda6c7c217b124aed30986cc3e865724834bd11
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-18 19:34:40 +0330

    chore: modify app.jsx and clean src folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Typography } from '@mui/material'
+
+function App() {
+
+  return (
+      <div>
+        <Typography variant='h1'> hello world! </Typography>
+      </div>
+  )
+}
+
+export default App
